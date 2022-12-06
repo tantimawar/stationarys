1ebb642bae92c13dc889d47884599d5bd685c1eb
tanti mawar <tantimawar04@gmail.com>
2022-12-06 09:56:45 +0700

    first commit

---
+++ nav.php
@@ -0,0 +1,15 @@
+<?php
+echo"<nav id='navbar' class='navbar order-last order-lg-0'>
+        <ul>
+          <li><a class='nav-link scrollto active' href='#hero'>Dashboard</a></li>
+          <li><a class='nav-link scrollto' href='#portfolio'>Kategori</a></li>
+          <li><a class='nav-link scrollto' href='./keranjang.html'>Keranjang</a></li>
+          <li class='dropdown'><a href='#'><span>Pesanan</span> <i class='bi bi-chevron-down'></i></a>
+            <ul>
+              <li><a href='#'>Drop Down</a></li>
+            </ul>
+          <li><a class='nav-link scrollto' href='#contact'>Akun</a></li>
+        </ul>
+        <i class='bi bi-list mobile-nav-toggle'></i>
+      </nav><!-- .navbar -->";
+?>
